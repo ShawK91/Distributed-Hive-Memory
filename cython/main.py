93e629355f4e035efe4db52f8b6e1cf534d4df54
Connor Yates <yatesco@oregonstate.edu>
2017-10-28 15:37:11 -0700

    Add back in the timing code

--- cython/main.py
+++ cython/main.py
@@ -11,7 +11,10 @@ else:
     gen_start = 1
 print 'Hive Memory Training with', parameters.num_input, 'inputs,', parameters.num_hnodes, 'hidden_nodes', parameters.num_output, 'outputs and', parameters.output_activation if parameters.output_activation == 'tanh' or parameters.output_activation == 'hardmax' else 'No output activation', 'Exp_opt:', '%.2f'%parameters.expected_optimal, 'Exp_min:', parameters.expected_min,'Exp_max:', parameters.expected_max
 sim_task = Task_Forage(parameters)
+start_time = time()
 for gen in range(gen_start, parameters.total_gens):
     best_train_fitness, validation_fitness = sim_task.evolve(gen, tracker)
     print 'Gen:', gen, 'Ep_best:', '%.2f' %best_train_fitness, ' Valid_Fit:', '%.2f' %validation_fitness, 'Cumul_valid:', '%.2f'%tracker.all_tracker[1][1], 'translated to', '%.2f'%tracker.all_tracker[2][1], 'out of', '%.2f'%parameters.expected_optimal_translated
     tracker.update([best_train_fitness, validation_fitness, (validation_fitness * (parameters.expected_max-parameters.expected_min)+parameters.expected_min)], gen)
+end_time = time()
+print "TOTAL TIME: " + str(end_time - start_time)
