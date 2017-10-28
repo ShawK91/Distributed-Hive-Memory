3918b870df07d0235f4754e04b0e3b970f77a53d
Connor Yates <yatesco@oregonstate.edu>
2017-10-28 16:33:34 -0700

    It compiles, but fails as it returns a single float to the hive_mem calls, and not an array?

--- cython/mod_hive_mem.pyx
+++ cython/mod_hive_mem.pyx
@@ -8,6 +8,66 @@ cimport numpy as np
 from scipy.special import expit
 from scipy.sparse import random as scipy_rand
 
+### BEGIN Custom code
+cdef np.ndarray[float] fast_hardmax(np.ndarray[float] layer_input):
+    return layer_input == np.max(layer_input)
+
+cdef void fast_graph_compute(
+        np.ndarray[float] input_v,
+        np.ndarray[float] memory,
+        np.ndarray[float] output,
+        np.ndarray[float] w_block_input_bias,
+        np.ndarray[float] w_hid_out,
+        np.ndarray[float] w_inp,
+        np.ndarray[float] w_inpgate,
+        np.ndarray[float] w_input_gate_bias,
+        np.ndarray[float] w_mem_inpgate,
+        np.ndarray[float] w_mem_readgate,
+        np.ndarray[float] w_mem_writegate,
+        np.ndarray[float] w_readgate,
+        np.ndarray[float] w_readgate_bias,
+        np.ndarray[float] w_rec_inp,
+        np.ndarray[float] w_rec_inpgate,
+        np.ndarray[float] w_rec_readgate,
+        np.ndarray[float] w_rec_writegate,
+        np.ndarray[float] w_writegate,
+        np.ndarray[float] w_writegate_bias,
+        str output_activation,
+        np.ndarray[float] result
+        ):
+    #Input gate
+    cdef np.ndarray[float] input_gate_out = expit(np.dot(input_v, w_inpgate) + np.dot(output, w_rec_inpgate) + np.dot(memory, w_mem_inpgate) + w_input_gate_bias)
+
+    #Input processing
+    cdef np.ndarray[float] block_input_out = expit(np.dot(input_v, w_inp) + np.dot(output, w_rec_inp) + w_block_input_bias)
+
+    #Gate the Block Input and compute the final input out
+    cdef np.ndarray[float] input_out = np.multiply(input_gate_out, block_input_out)
+
+    #Read Gate
+    cdef np.ndarray[float] read_gate_out = expit(np.dot(input_v, w_readgate) + np.dot(output, w_rec_readgate) + np.dot(memory, w_mem_readgate) + w_readgate_bias)
+
+    #Compute hidden activation - processing hidden output for this iteration of net run
+    cdef np.ndarray[float] hidden_act = np.multiply(read_gate_out, memory) + input_out
+
+    #Write gate (memory cell)
+    cdef np.ndarray[float] write_gate_out = expit(np.dot(input_v, w_writegate)+ np.dot(output, w_rec_writegate) + np.dot(memory, w_mem_writegate) + w_writegate_bias)
+
+    #Write to memory Cell - Update memory
+    memory += np.multiply(write_gate_out, np.tanh(hidden_act))
+
+    #Compute final output
+    output = np.dot(hidden_act, w_hid_out)
+
+    if output_activation == 'tanh':
+        output = np.tanh(output)
+    elif output_activation == 'hardmax':
+        output = fast_hardmax(output)
+
+    #return np.array(output)
+    result = np.array(output)
+### END CUSTOM CODE
+
 
 class Drone_Default:
     def __init__(self, params, drone_id, num_input, num_hnodes, num_mem, num_output):
@@ -117,36 +177,11 @@ class Drone_Default:
 
         return np.array(self.output).tolist(), memory
 
-    def graph_compute(self, input, memory): #Feedforwards the input and computes the forward pass of the network
-        input = np.mat(input)
-
-        #Input gate
-        input_gate_out = expit(np.dot(input, self.w_inpgate) + np.dot(self.output, self.w_rec_inpgate) + np.dot(memory, self.w_mem_inpgate) + self.w_input_gate_bias)
-
-        #Input processing
-        block_input_out = expit(np.dot(input, self.w_inp) + np.dot(self.output, self.w_rec_inp) + self.w_block_input_bias)
-
-        #Gate the Block Input and compute the final input out
-        input_out = np.multiply(input_gate_out, block_input_out)
-
-        #Read Gate
-        read_gate_out = expit(np.dot(input, self.w_readgate) + np.dot(self.output, self.w_rec_readgate) + np.dot(memory, self.w_mem_readgate) + self.w_readgate_bias)
-
-        #Compute hidden activation - processing hidden output for this iteration of net run
-        hidden_act = np.multiply(read_gate_out, memory) + input_out
-
-        #Write gate (memory cell)
-        write_gate_out = expit(np.dot(input, self.w_writegate)+ np.dot(self.output, self.w_rec_writegate) + np.dot(memory, self.w_mem_writegate) + self.w_writegate_bias)
-
-        #Write to memory Cell - Update memory
-        memory += np.multiply(write_gate_out, np.tanh(hidden_act))
-
-        #Compute final output
-        self.output = np.dot(hidden_act, self.w_hid_out)
-        if self.params.output_activation == 'tanh': self.output = np.tanh(self.output)
-        elif self.params.output_activation == 'hardmax': self.output = self.hardmax(self.output)
-
-        return np.array(self.output).tolist()
+    def graph_compute(self, input_v, memory): #Feedforwards the input and computes the forward pass of the network
+        input_v = np.mat(input_v)
+        result = np.zeros(1)
+        fast_graph_compute( input_v, memory, self.output, self.w_block_input_bias, self.w_hid_out, self.w_inp, self.w_inpgate, self.w_input_gate_bias, self.w_mem_inpgate, self.w_mem_readgate, self.w_mem_writegate, self.w_readgate, self.w_readgate_bias, self.w_rec_inp, self.w_rec_inpgate, self.w_rec_readgate, self.w_rec_writegate, self.w_writegate, self.w_writegate_bias, self.params.output_activation, result)
+        return result.tolist()
 
     def reset(self):
         self.output = np.mat(np.zeros((1,self.num_output)))
--- cython/setup.py
+++ cython/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-modules = ["mod_hive_mem.pyx", "hive_mem.pyx"]
+modules = ["mod_hive_mem.pyx", "hive_mem.pyx", "mod_hive_mem_funcs.pyx"]
 setup(
     ext_modules = cythonize(modules)
 )
